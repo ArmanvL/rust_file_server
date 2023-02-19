634661b55464c64ad08806c67a2598b87a46c4c7
Arman van Lieshout <arman.vanlieshout@cm.com>
2023-02-19 22:16:17 +0100

    wip: added a get file endpoint, working on decryption of stored file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use actix_web::{
-    post,
-    Error,
-    App,
-    HttpResponse,
-    HttpServer,
-    http::header::ContentDisposition, error,
-};
-use actix_multipart::Multipart;
-use futures_util::stream::TryStreamExt;
 use std::fs::File;
+use std::io::Read;
 use std::io::Write;
 use std::path::Path;
+use actix_web::{
+    post,
+    get,
+    Error,
+    App,
+    Responder,
+    HttpServer,
+    http::header::ContentDisposition,
+    error,
+    web::{
+        Query,
+        Json
+    }
+};
+use actix_multipart::Multipart;
+use serde::{
+    Deserialize,
+    Serialize
+};
+use futures_util::stream::TryStreamExt;
+use uuid::Uuid;
 use aes_gcm::{
     aead::{
         Aead,
@@ -24,8 +36,21 @@ use aes_gcm::{
 // Make this configurable through an environment variable
 const FILE_SIZE_LIMIT: usize = 5_000_000;
 
+#[derive(Debug, Serialize)]
+struct UploadResponse {
+    files: Vec<UploadedFile>,
+}
+
+#[derive(Debug, Serialize)]
+struct UploadedFile {
+    id: Uuid,
+    original_file_name: String,
+}
+
 #[post("/upload")]
-async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
+async fn upload(mut payload: Multipart) -> Result<impl Responder, Error> {
+    let mut uploaded_files: Vec<UploadedFile> = vec![];
+
     // Take the next field from the multipart stream
     while let Some(mut field) = payload.try_next().await? {
         let mut bytes = Vec::<u8>::new();
@@ -56,7 +81,8 @@ async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
             None => return Err(error::ErrorBadRequest("Empty/no file name")),
         }
 
-        let path = format!("./files/{}", name.unwrap()).to_string();
+        let id = Uuid::new_v4();
+        let path = format!("./files/{}", id).to_string();
         let path = Path::new(&path);
         let display = path.display();
 
@@ -79,9 +105,43 @@ async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
         } else {
             unreachable!("Something stupid happened here, I should learn how to Rust better");
         }
+
+        uploaded_files.push(UploadedFile { id, original_file_name: name.unwrap().to_string() })
     }
 
-    return Ok(HttpResponse::Ok().body("Yes"));
+    let response: UploadResponse = UploadResponse { files: uploaded_files };
+
+    return Ok(Json(response));
+}
+
+#[derive(Debug, Deserialize)]
+struct GetFileRequesst {
+    id: String,
+    // todo: Add authentication token?
+}
+
+#[get("/file")]
+async fn get_file(params: Query<GetFileRequesst>) -> Result<impl Responder, Error> {
+    let path = format!("./files/{}", params.id).to_string();
+    let path = Path::new(&path);
+
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce");
+
+    let mut file = File::open(path)?;
+    let mut buffer = Vec::<u8>::new();
+    file.read_to_end(&mut buffer)?;
+
+    let buffer = &buffer[..];
+    let decrypted = cipher.decrypt(nonce, buffer);
+
+    match decrypted {
+        Ok(val) => println!("{:?}", val),
+        Err(err) => println!("{:?}", err),
+    };
+    
+    return Ok("OK");
 }
 
 #[actix_web::main]
@@ -89,6 +149,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(upload)
+            .service(get_file)
     })
         .bind(("127.0.0.1", 3000))?
         .run()
